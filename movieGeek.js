2adbb09b013f2fbf3b697f0c92014be5818ce28e
Александр <joedassin2015@gmail.com>
2024-01-08 21:04:25 +0300

    Added automation in project

--- movieGeek.js
+++ movieGeek.js
@@ -1,4 +1,7 @@
-let numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", "");
+let numberOfFilms;
+
+numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", "");
+
 
 const personalMovieDB = {
 	count : numberOfFilms,
@@ -8,9 +11,31 @@ const personalMovieDB = {
 	privat:false
 }; 
 
-let lastWatchedFilm= prompt("Последний просмотренный фильм", ""),
-	raitingWatchedFilm = prompt("Оценка фильма", "");
 
-personalMovieDB.movies[lastWatchedFilm] = raitingWatchedFilm;
+for(let i = 0; i < 2; i++){
+	let	lastWatchedFilm = prompt("Последний просмотренный фильм", "");
+	let raitingWatchedFilm = prompt("Оценка фильма", "");
 
+	if(lastWatchedFilm != null && raitingWatchedFilm != null 
+		&& lastWatchedFilm != "" && raitingWatchedFilm != "" && lastWatchedFilm.length < 50){
+			personalMovieDB.movies[lastWatchedFilm] = raitingWatchedFilm;
+			console.log("Done!");
+		}else {
+			i--;
+			console.log("It's not done!");
+		}
+}
 console.log(personalMovieDB);
+
+alert("Исходя из вышеперечисленного определим кто ты по жизнги...");
+let whoAreYouInLife = personalMovieDB.count;
+
+if(whoAreYouInLife > 1 && whoAreYouInLife < 10){
+	alert("Щегол");
+}else if(whoAreYouInLife > 10 && whoAreYouInLife < 30){
+	alert("Юный петушара");
+}else if (whoAreYouInLife > 30){
+	alert("Петушара");
+}else {
+	alert("Лох не из нашей песочницы");
+}
