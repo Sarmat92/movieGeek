b8337aba00f52781406aae4f74b74c2d5356e0eb
Александр <joedassin2015@gmail.com>
2024-01-15 07:30:28 +0300

    Added functions

--- movieGeek.js
+++ movieGeek.js
@@ -1,7 +1,16 @@
 let numberOfFilms;
 
-numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", "");
+numberOfFilms;
 
+function start(){
+	numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", "");
+
+	while(numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)){
+		numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", "");
+	}
+}
+
+start();
 
 const personalMovieDB = {
 	count : numberOfFilms,
@@ -12,30 +21,63 @@ const personalMovieDB = {
 }; 
 
 
-for(let i = 0; i < 2; i++){
-	let	lastWatchedFilm = prompt("Последний просмотренный фильм", "");
-	let raitingWatchedFilm = prompt("Оценка фильма", "");
 
-	if(lastWatchedFilm != null && raitingWatchedFilm != null 
-		&& lastWatchedFilm != "" && raitingWatchedFilm != "" && lastWatchedFilm.length < 50){
-			personalMovieDB.movies[lastWatchedFilm] = raitingWatchedFilm;
-			console.log("Done!");
-		}else {
+function rememberMyFilms(){
+	for(let i = 0; i < 2; i++){
+		let	lastWatchedFilm = prompt("Последний просмотренный фильм", "");
+		let raitingWatchedFilm = prompt("Оценка фильма", "");
+	
+		if(lastWatchedFilm != null && raitingWatchedFilm != null 
+			&& lastWatchedFilm != "" && raitingWatchedFilm != "" && lastWatchedFilm.length < 50){
+				personalMovieDB.movies[lastWatchedFilm] = raitingWatchedFilm;
+				console.log("Done!");
+			}else {
+				i--;
+				console.log("It's not done!");
+			}
+	}
+}
+
+//rememberMyFilms();
+
+
+
+function showPersonalLevel(){
+	alert("Исходя из вышеперечисленного определим кто ты по жизнги...");
+	let whoAreYouInLife = personalMovieDB.count;
+
+	if(whoAreYouInLife > 1 && whoAreYouInLife < 10){
+		alert("Щегол");
+	}else if(whoAreYouInLife > 10 && whoAreYouInLife < 30){
+		alert("Юный петушара");
+	}else if (whoAreYouInLife > 30){
+		alert("Петушара");
+	}else {
+		alert("Лох не из нашей песочницы");
+	}
+}
+
+//showPersonalLevel();
+
+function writeYourFavoriteGanres(){
+	
+	for(let i = 0; i < 3; i++){
+		let favoriteGanre = prompt("Your favoritr genre of film?", "");
+
+		if(favoriteGanre != null && favoriteGanre != ""){
+			personalMovieDB.genres[i] = favoriteGanre;
+		}else{
 			i--;
-			console.log("It's not done!");
 		}
+	}
 }
-console.log(personalMovieDB);
 
-alert("Исходя из вышеперечисленного определим кто ты по жизнги...");
-let whoAreYouInLife = personalMovieDB.count;
+//writeYourFavoriteGanres();
 
-if(whoAreYouInLife > 1 && whoAreYouInLife < 10){
-	alert("Щегол");
-}else if(whoAreYouInLife > 10 && whoAreYouInLife < 30){
-	alert("Юный петушара");
-}else if (whoAreYouInLife > 30){
-	alert("Петушара");
-}else {
-	alert("Лох не из нашей песочницы");
+function showMyDB(){
+	if(personalMovieDB.privat != true){
+		console.log(personalMovieDB);
+	}
 }
+
+showMyDB();
